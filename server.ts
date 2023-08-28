9bf2128ea2f3c6d971b0ce6412c99788661ff405
agtTwilight <lukasmacmillen@gmail.com>
2023-08-28 14:23:21 -0700

    modified setup init for packagejson

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-const port = '3080';
+const port = '8000';
 
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello from TS, with watchmode!');
+	res.send('Hello from TS, with ts-node!');
 });
 
 app.listen(port, () => {
-	console.log(`listining on port localhost:${port}`);
+	console.log(`listining on port: ${port}`);
 });
