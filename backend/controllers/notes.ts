419b4771f67fe774860c191d2d7296399de3d598
agtTwilight <lukasmacmillen@gmail.com>
2023-09-02 14:39:59 -0700

    setup update and delete routes for notes by id, with error handling

--- backend/controllers/notes.ts
+++ backend/controllers/notes.ts
@@ -23,9 +23,8 @@ export const getNote: RequestHandler = async (req, res, next) => {
 
 		const note = await NoteModel.findById(noteId).exec();
 
-		if (!note) {
-			throw createHttpError(404, `Note ${noteId} not found.`);
-		}
+		if (!note) throw createHttpError(404, `Note ${noteId} not found.`);
+
 		res.status(200).json(note);
 	} catch (error) {
 		next(error);
@@ -47,18 +46,79 @@ export const createNote: RequestHandler<
 > = async (req, res, next) => {
 	const title = req.body.title;
 	const text = req.body.text;
+	const priority = req.body.priority;
 
 	try {
-		if (!title) {
-			throw createHttpError(400, 'Notes must have a title');
-		}
+		if (!title) throw createHttpError(400, 'Notes must have a title');
 
 		const newNote = await NoteModel.create({
 			title,
 			text,
+			priority,
 		});
 
 		res.status(201).json(newNote);
+	} catch (error) {
+		next(error);
+	}
+};
+
+interface UpdateNoteParams {
+	noteId: string;
+}
+
+interface UpdateNoteBody {
+	title?: string;
+	text?: string;
+	completed?: boolean;
+	priority?: number;
+}
+
+export const updateNote: RequestHandler<
+	UpdateNoteParams,
+	unknown,
+	UpdateNoteBody,
+	unknown
+> = async (req, res, next) => {
+	const noteId = req.params.noteId;
+	const newTitle = req.body.title;
+	const newText = req.body.text;
+	const newCompleted = req.body.completed;
+	const newPriority = req.body.priority;
+
+	try {
+		if (!mongoose.isValidObjectId(noteId)) {
+			throw createHttpError(400, `Invalid note id, ${noteId}`);
+		}
+
+		const note = await NoteModel.findById(noteId).exec();
+
+		if (!note) throw createHttpError(404, `Note ${noteId} not found.`);
+
+		if (newTitle) note.title = newTitle;
+		if (newText) note.text = newText;
+		if (newCompleted) note.completed = newCompleted;
+		if (newPriority) note.priority = newPriority;
+
+		const updatedNote = await note.save();
+
+		res.status(200).json(updatedNote);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+	const noteId = req.params.noteId;
+
+	try {
+		if (!mongoose.isValidObjectId(noteId)) {
+			throw createHttpError(400, `Invalid note id, ${noteId}`);
+		}
+
+		await NoteModel.findOneAndDelete({ _id: noteId });
+
+		res.sendStatus(204);
 	} catch (error) {
 		next(error);
 	}
